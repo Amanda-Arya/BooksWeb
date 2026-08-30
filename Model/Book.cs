1a0a3ff2c6219d201187d45d7f42cedf28b038b5
-Amanda <amandacvergilio.eng@hotmail.com>
2026-08-30 20:04:35 -0300

    criando model livros

--- Model/Book.cs
+++ Model/Book.cs
@@ -1,6 +1,33 @@
-﻿namespace BooksWeb.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BooksWeb.Model
 {
+    [Table("Books")]
     public class Book
     {
+        [Required]
+        [Column("title", TypeName = "varchar(max)")]
+        public string Title { get; set; }
+        [Required]
+        [Column("author", TypeName = "varchar(50)")]
+        public string Author { get; set; }
+        [Required]
+        [Column("gender", TypeName = "varchar(50)")]
+        public string Gender { get; set; }
+        [Required]
+        [Column("publisher", TypeName = "varchar(50)")]
+        public string Publisher { get; set; }
+        [Required]
+        [Column("year", TypeName = "int")]
+        public int Year { get; set; }
+        [Required]
+        [Column("pagesNumber", TypeName = "int")]
+        public int PagesNUmber { get; set; }
+        [Required]
+        [Column("ISBN", TypeName = "int")]
+        public int ISBN { get; set; }
+
+
     }
 }
--- obj/Debug/net10.0/BooksWeb.AssemblyInfo.cs
+++ obj/Debug/net10.0/BooksWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BooksWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9df9d176534fae421049bce9360447f659d333d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BooksWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BooksWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
